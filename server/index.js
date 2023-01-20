e8884b387a9d0c20663786f2bfff1c11c7fba2bc
Ugo <ugo.boulestreau@epitech.eu>
2023-01-20 16:55:20 +0100

    [FIX]: cors was not correctly configured

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const about_json = require('../about.json')
+const about_json = require('./about.json')
 const PORT = process.env.PORT || 8080;
 
 var corsOptions = {
@@ -13,6 +13,21 @@ app.use(cors(corsOptions));
 app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
+
+//cors basic config
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+
 
 app.get("/", (req, res) => {
   res.send("Hello world !");
