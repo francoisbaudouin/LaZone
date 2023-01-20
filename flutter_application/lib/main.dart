f592caac136bb6d65101b69371d2b2dcda9713d4
Alexandre Tomasin <alexandre.tomasin@epitech.eu>
2023-01-20 14:34:59 +0100

    [ADD]: responsive features

--- flutter_application/lib/main.dart
+++ flutter_application/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,6 +12,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      builder: (context, widget) => ResponsiveWrapper.builder(
+        ClampingScrollWrapper.builder(context, widget!),
+        breakpoints: const[
+          ResponsiveBreakpoint.resize(350, name: MOBILE),
+          ResponsiveBreakpoint.autoScale(600, name: TABLET),
+          ResponsiveBreakpoint.resize(800, name: DESKTOP),
+          ResponsiveBreakpoint.autoScale(1700, name: 'XL'),
+        ],
+      ),
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
