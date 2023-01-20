55d3268d335b05b442e200f6ed1731f9e1f63fcd
Alexandre Tomasin <alexandre.tomasin@epitech.eu>
2023-01-20 17:42:42 +0100

    [FIX]: remove useless comments and dependencies

--- flutter_application/lib/main.dart
+++ flutter_application/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_framework/responsive_framework.dart';
 import 'login_page.dart';
-import 'dart:io';
 import 'package:http/http.dart';
 
 void main() {
@@ -62,19 +61,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
   String serverResponse = 'Server response';
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -122,7 +109,6 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _makeGetRequest,
-        // tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
