4ae2a768caaf18ba7e5b29d758452341321a793b
Ugo <ugo.boulestreau@epitech.eu>
2023-02-05 23:49:13 +0100

    [ADD]: basic config file to test db

---
+++ server/api/utils/basicConfig.js
@@ -0,0 +1,90 @@
+const { PrismaClient } = require("@prisma/client");
+const { Strategy } = require("passport-local");
+const { hash, compare } = require("../utils/utils");
+const passport = require("passport");
+require("dotenv").config();
+
+// Initialize a prisma client
+const prisma = new PrismaClient();
+
+async function basicConfig() {
+  const user = await prisma.users.create({
+    data: {
+      email: "test@testmail.com",
+      password: await hash("password"),
+      lastname: "test",
+      firstname: "Tets",
+      pseudo: "mypseudo",
+    }
+  })
+  const GitService = await prisma.services.create({
+    data: {
+      name: "Github",
+      description: "This is git service",
+    }
+  })
+  const DiscordService = await prisma.services.create({
+    data: {
+      name: "Discord",
+      description: "This is discord service",
+    }
+  })
+  const DiscordToken = await prisma.tokens.create({
+    data: {
+      accessTokens: "Discord accessTokens",
+      refreshTokens: "Discord refreshTokens",
+      relatedServiceName: "Discord",
+      userId: 1
+    }
+  })
+  const GithubToken = await prisma.tokens.create({
+    data: {
+      accessTokens: "Github accessTokens",
+      refreshTokens: "Github refreshTokens",
+      relatedServiceName: "Github",
+      userId: 1
+    }
+  })
+  const pullRequestAction = await prisma.actions.create({
+    data: {
+      name: "pull request",
+      description: "pull request occured",
+      serviceName: "Github"
+    }
+  })
+  const newIssueAction = await prisma.actions.create({
+    data: {
+      name: "new issue",
+      description: "new issue occured",
+      serviceName: "Github"
+
+    }
+  })
+  const newMessageReaction = await prisma.reactions.create({
+    data: {
+      name: "DiscordMessage",
+      description: "discord message reaction",
+      serviceName: "Discord"
+
+    }
+  })
+  const reactReaction = await prisma.reactions.create({
+    data: {
+      name: "discord reaction emoji",
+      description: "put an emoji",
+      serviceName: "Discord"
+    }
+  })
+  const newAreas = await prisma.areas.create({
+    data: {
+      userId: 1,
+      actionsId: 1,
+      reactionsId: 1,
+      enabled: true,
+      actionsParams: "Your Action Params",
+      reactionsParams: "Your Reaction Params"
+    }
+  })
+}
+
+module.exports = basicConfig;
