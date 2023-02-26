7236f62260b5e0f766b351eeae64fb9bc4400afb
Ugo <ugo.boulestreau@epitech.eu>
2023-02-26 15:36:57 +0100

    [ADD]: basicConfig  epuration + verification of connected user

--- server/api/utils/basicConfig.js
+++ server/api/utils/basicConfig.js
@@ -8,16 +8,6 @@ require("dotenv").config();
 const prisma = new PrismaClient();
 
 async function basicConfig() {
-  await prisma.users.create({
-    data: {
-      email: "test@testmail.com",
-      password: await hash("password"),
-      lastname: "test",
-      firstname: "Tets",
-      pseudo: "mypseudo",
-    }
-  })
-
   await prisma.services.create({
     data: {
       name: "Github",
@@ -30,24 +20,6 @@ async function basicConfig() {
       description: "This is discord service",
     }
   })
-
-  await prisma.tokens.create({
-    data: {
-      accessTokens: process.env.DISCORD_BOT_TOKEN,
-      refreshTokens: "Discord refreshTokens",
-      relatedServiceName: "Discord",
-      userId: 1
-    }
-  })
-  await prisma.tokens.create({
-    data: {
-      accessTokens: process.env.GIT_TOKEN,
-      refreshTokens: "Github refreshTokens",
-      relatedServiceName: "Github",
-      userId: 1
-    }
-  })
-
   await prisma.actions.create({
     data: {
       name: "new issue",
@@ -69,7 +41,6 @@ async function basicConfig() {
       serviceName: "Github"
     }
   })
-
   await prisma.reactions.create({
     data: {
       name: "DiscordMessage",
@@ -90,47 +61,6 @@ async function basicConfig() {
       name: "DiscordCategory",
       description: "create a category",
       serviceName: "Discord"
-    }
-  })
-
-  await prisma.areas.create({
-    data: {
-      userId: 1,
-      actionsId: 1,
-      reactionsId: 1,
-      enabled: true,
-      actionsParams: "UgoBoulestreau/POC-nodejs",
-      reactionsParams: "1067077985213100134"
-    }
-  })
-  await prisma.areas.create({
-    data: {
-      userId: 1,
-      actionsId: 2,
-      reactionsId: 3,
-      enabled: true,
-      actionsParams: "UgoBoulestreau/POC-nodejs",
-      reactionsParams: "693113343153537035"
-    }
-  })
-  await prisma.areas.create({
-    data: {
-      userId: 1,
-      actionsId: 3,
-      reactionsId: 2,
-      enabled: true,
-      actionsParams: "UgoBoulestreau/POC-nodejs",
-      reactionsParams: "693113343153537035"
-    }
-  })
-  await prisma.areas.create({
-    data: {
-      userId: 1,
-      actionsId: 1,
-      reactionsId: 2,
-      enabled: true,
-      actionsParams: "UgoBoulestreau/POC-nodejs",
-      reactionsParams: "693113343153537035"
     }
   })
 }
