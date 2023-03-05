dffd46328955338ce5bcf5d1dcfec47657e2c67e
Ugo <ugo.boulestreau@epitech.eu>
2023-03-05 01:36:07 +0100

    [ADD]: readd some weird flutter things

--- flutter_application/windows/flutter/generated_plugin_registrant.cc
+++ flutter_application/windows/flutter/generated_plugin_registrant.cc
@@ -6,8 +6,11 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <flutter_secure_storage_windows/flutter_secure_storage_windows_plugin.h>
 #include <url_launcher_windows/url_launcher_windows.h>
 
+FlutterSecureStorageWindowsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterSecureStorageWindowsPlugin"));
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   UrlLauncherWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("UrlLauncherWindows"));
